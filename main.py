cd13936170e0a0d60303fe32506a59f481a3d4d3
LOL-xt <cohenjoni10@gmail.com>
2022-04-23 19:21:53 +0300

    finished server!

--- main.py
+++ main.py
@@ -214,9 +214,9 @@ def find_opponent(waiting_room):
 @app.on_event("startup")
 @repeat_every(seconds=3, wait_first=True)
 async def match():
-    print("match")
     for waiting_room in waiting_rooms:
         if waiting_room.qsize() >= MINIMUM_USERS_IN_WAITING_ROOM:
+            print("match")
             (user1, user2) = find_opponent(waiting_room)
             if abs(user1["rank"] - user2["rank"]) <= MAX_RANK_DIFFERENCE or (
                     user1["waiting_time"] > MAX_WAITING_TIME and user2["waiting_time"] > MAX_WAITING_TIME):
